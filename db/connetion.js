d78290453708a7a762d638dd2e1931953598a222
julispeed <julian.acuna2003@gmail.com>
2025-09-07 12:59:34 -0300

    falopa9

--- db/connetion.js
+++ db/connetion.js
@@ -7,11 +7,11 @@ let pool;
 
 try {
   pool = mysql.createPool({
-    host: process.env.MYSQLHOST,
-    port: process.env.MYSQLPORT,
-    user: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDATABASE,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
