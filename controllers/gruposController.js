acebd6fb273c74e1748deb2f11b5e28214637ea5
julispeed <julian.acuna2003@gmail.com>
2025-09-09 19:25:59 -0300

    Deploy 1 groups

--- controllers/gruposController.js
+++ controllers/gruposController.js
@@ -1,83 +1,77 @@
 import db from '../db/connetion.js';
-
-
-
-const crearGrupo = (req, res) => {
-  const {
-    Nombre,
-    Descripcion,
-    IdFamilia } = req.body;
-
-    console.log('Datos recibidos:', req.body);
-
+const crearGrupo = async (req, res) => {
+  const { Nombre, Descripcion, IdFamilia } = req.body;
+  console.log('Datos recibidos:', req.body);
   const sql = `
     INSERT INTO GruposArticulos
     (Nombre, Descripcion, IdFamilia)
     VALUES (?, ?,?)
     `;  
-
-const values = [Nombre,Descripcion,IdFamilia];    
-    
-  db.query(sql, values, (err, result) => {
-    if (err) {
-  console.error('Error al insertar grupo:', err);
-  if (err.code==='ER_DUP_ENTRY') {
+  const values = [Nombre,Descripcion,IdFamilia];    
+  
+  try {
+    const [result] = await db.query(sql, values);
+    res.status(201).json({ message: 'Grupo Insertado', id: result.insertId });
+  }
+  catch (err) {
+    console.error('Error al insertar grupo:', err);
+    if (err.code==='ER_DUP_ENTRY') {
       return res.status(400).json({message: 'Ya existe un grupo con ese nombre'});
     }
-  return res.status(500).send(err);
-}
-    res.status(201).json({ message: 'Grupo Insertado', id: result.insertId });    
-  });
+    return res.status(500).send(err);
+  }
 };
 
-const obtenerGrupos = (req, res) => {
+const obtenerGrupos = async (req, res) => {
   const sql = 'SELECT IdGrupoArticulo, Nombre, Descripcion, IdFamilia FROM GruposArticulos';
-  db.query(sql, (err, results) => {
-    if (err) return res.status(500).send(err);
-    res.json(results);
-  });
+  try {
+      const [results] = await db.query(sql);
+      res.json(results);
+  }
+  catch(err) {
+     console.error('Error al obtener grupos:', err);
+    res.status(500).json({ error: err.message });
+  }
+
 };
 
-const actualizarGrupo = (req, res) => {
+const actualizarGrupo = async (req, res) => {
   const { id } = req.params;
-  const {
-    Nombre,
-    Descripcion,            
-    IdFamilia
-  } = req.body;  
-
+  const {Nombre, Descripcion, IdFamilia} = req.body;  
   const sql = `
     UPDATE GruposArticulos
     SET Nombre = ? ,  Descripcion = ?,  IdFamilia = ?
     WHERE IdGrupoArticulo = ?
   `;
-
   const values = [Nombre, Descripcion, IdFamilia, id];
 
-  db.query(sql, values, (err) => {
-    if (err) {
+try {
+  await db.query(sql, values);
+  res.json({ message: 'Grupo actualizado correctamente' });
+}
+catch (err) {
       console.error('Error al actualizar grupo:', err);
       return res.status(500).json({ message: 'Error interno del servidor' });
-    }
-    res.json({ message: 'Grupo actualizado correctamente' });
-  });
+}
+
 };
 
-const eliminarGrupo = (req, res) => {
+const eliminarGrupo = async (req, res) => {
       const sql = `
       DELETE FROM GruposArticulos    
       WHERE IdGrupoArticulo = ?
       `;
   const values=req.params.id;  
-  db.query(sql, values, (err) => {
-    if (err) {
+
+  try {
+    await db.query(sql, values);
+    res.status(201).json({ message: 'Grupo eliminado'});
+  }
+  catch (err) {
       console.error('Error al eliminar grupo:', err);
       return res.status(500).json({ message: 'Error interno del servidor' });
-    }
-  else
-  {
-    res.status(201).json({ message: 'Grupo eliminado'});
-  }})
+  }
+
 }
 
 export  {
