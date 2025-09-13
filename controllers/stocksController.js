3d47e2e3ea6612d7847a33e442f0cc7f0fb6085d
julispeed <julian.acuna2003@gmail.com>
2025-09-13 14:59:36 -0300

    listado 2

--- controllers/stocksController.js
+++ controllers/stocksController.js
@@ -1,50 +1,45 @@
 import db from '../db/connetion.js';
 
-
-
 const stocksArticulos = async (req, res) => {
-  const sql = 
-  `
-SELECT
-    a.IdArticulo,
-    a.Codigo,
-    a.Descripcion,
-    a.Unidad_medida,
-    a.Codigo_barra,
-    a.Precio,
-    a.Costo,
-    a.IdGrupoArticulos,
-    COALESCE(SUM(
-        CASE 
-            WHEN m.TipoMovimiento IN ('ING','DEV') THEN md.Cantidad
-            WHEN m.TipoMovimiento IN ('EGR','VEN') THEN -md.Cantidad
-            ELSE 0
-        END
-    ), 0) AS StockActual
-FROM Articulos a
-LEFT JOIN MovimientosDetalleStock md ON a.IdArticulo = md.IdArticulo
-LEFT JOIN MovimientosStock m ON md.IdMovimientoStock = m.IdMovimientoStock
-GROUP BY 
-    a.IdArticulo,
-    a.Codigo,
-    a.Descripcion,
-    a.Unidad_medida,
-    a.Codigo_barra,
-    a.Precio,
-    a.Costo,
-    a.IdGrupoArticulos
-ORDER BY a.Descripcion;
+  const sql = `
+    SELECT
+        a.IdArticulo,
+        a.Codigo,
+        a.Descripcion,
+        a.Unidad_medida,
+        a.Codigo_barra,
+        a.Precio,
+        a.Costo,
+        a.IdGrupoArticulos,
+        COALESCE(SUM(
+            CASE 
+                WHEN m.TipoMovimiento IN ('ING','DEV') THEN md.Cantidad
+                WHEN m.TipoMovimiento IN ('EGR','VEN') THEN -md.Cantidad
+                ELSE 0
+            END
+        ), 0) AS StockActual
+    FROM Articulos a
+    LEFT JOIN MovimientosDetalleStock md ON a.IdArticulo = md.IdArticulo
+    LEFT JOIN MovimientosStock m ON md.IdMovimientoStock = m.IdMovimientoStock
+    GROUP BY 
+        a.IdArticulo,
+        a.Codigo,
+        a.Descripcion,
+        a.Unidad_medida,
+        a.Codigo_barra,
+        a.Precio,
+        a.Costo,
+        a.IdGrupoArticulos
+    ORDER BY a.Descripcion;
   `;
+
   try {
-      db.query(sql);
-      res.json(results);
-  }
-  catch (err) {
+    const [results] = await db.query(sql);
+    res.json(results);
+  } catch (err) {
     console.error("Error en listado:", err);  
-    res.status(500).json({ message: 'Error en el servidor', error: err });
+    res.status(500).json({ message: "Error en el servidor" });
   }
 };
 
-export  {
-  stocksArticulos,
-}
+export { stocksArticulos };
